13223b93f738045a14a94b45d7dfcd404b2d7064
darlanmarangoni <marangoni.darlan1@gmail.com>
2020-07-28 22:37:01 -0300

    first componet

---
+++ src/components/components/ButtonLink/index.js
@@ -0,0 +1,13 @@
+import React, { Children } from 'react';
+
+function ButtonLink (props) {
+    //props =: { className: "o que alguem passar", href: "/"}
+
+    return (
+        <a className={props.className} href={props.href}>
+            {props.children}
+        </a>
+    );
+}
+
+export default ButtonLink;
